c918e43bca2ef19f7b8b34fcf6fb3614fdf42634
your_github_username <muhammad.usama12@hotmail.com>
2022-10-13 22:42:01 +0000

    add ajax post and get request, update html ids

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+$(document).ready(function () {
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
     },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+  ];
 
-renderTweets = function (tweets) {
-  // loops through tweets
-  for (let tweet of tweets) {
-    // calls createtweet elemnt for each tweet
-    const $tweet = createTweetElement(tweet);
+  renderTweets = function (tweets) {
+    $("#tweets-container").html("");
+    for (let tweet of tweets) {
+      // calls createtweet elemnt for each tweet
+      const $tweet = createTweetElement(tweet);
 
-    $(document).ready(function () {
-      $("#tweets-container").append($tweet);
+      $("#tweets-container").prepend($tweet);
       // takes return value and appends it to the tweets container
-    });
-  }
-};
+    }
+  };
 
-createTweetElement = function (tweetObj) {
-  const $tweet = $(`
+  createTweetElement = function (tweetObj) {
+    const $tweet = $(`
 <article class="tweet">
           <header>
             <h2><img src="${tweetObj.user.avatars}"></i>${tweetObj.user.name}</h2>
@@ -59,26 +58,64 @@ createTweetElement = function (tweetObj) {
           </footer>
         </article>
         `);
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+  renderTweets(data);
 
-// add an event listener that listens for the submit event
+  // add an event listener that listens for the submit event
 
-// prevent the default behaviour of submit event
-$("#new-tweet").on("click", function (event) {
-  event.preventDefault();
-});
+  // prevent the default behaviour of submit event
 
-$(document).ready(function () {
-  $("button").click(function () {
-    $("data").text($("form").serializae());
+  $("#new-tweet").on("submit", function (event) {
+    event.preventDefault();
+    const seralizedData = $(this).serialize();
+    $.ajax({
+      url: `/tweets`,
+      method: "POST",
+      data: seralizedData,
+    })
+      .then(function () {
+        $.ajax({
+          url: `/tweets`,
+          method: "GET",
+        }).then(function (result) {
+          console.log(result);
+          renderTweets(result);
+        });
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   });
+
+  const loadTweet = function () {
+    $.ajax({
+      url: `/tweets`,
+      method: "GET",
+    })
+      .then(function (result) {
+        console.log(result);
+        renderTweets(result);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+  loadTweet();
 });
+
+// $("button").click(function () {
+//   $("data").text($("form").serializae());
+// });
 
 // create an AJAX POST request in client.js that sends the form data to the server
 
-const tweetReq = function () {
-  $.ajax({ url: `/tweets/`, method: "POST" });
-};
+// const $button = $("tweet-submission");
+// $button.on(click, function () {
+//   console.log("Performing ajax call");
+//   $.ajax("index.html", { method: "GET" }).then(function (newTweetPosts) {
+//     console.log("Success: ", newTweetPosts);
+//     $button.replaceWith(newTweetPosts);
+//   });
+// });
